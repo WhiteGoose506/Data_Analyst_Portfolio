2b0505e572e49cf2a5480da9657b03f9eeaa7a29
Haizan <haizan001@hotmail.com>
2025-08-19 17:32:18 +0800

    first_question_completed

--- Capstone_Data_Job_Analysis/1_Highest_paying_jobs.sql
+++ Capstone_Data_Job_Analysis/1_Highest_paying_jobs.sql
@@ -5,3 +5,74 @@ Question: What are the top-paying data analyst jobs?
 - BONUS: Include company names of top 10 roles
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
 */
+
+--With CTE (For practice purpose, I understand my query is not repetitively used)
+
+WITH top_ten_highest_pay AS
+(
+    SELECT
+        company_id,
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date
+    FROM
+        Job_postings_fact
+    WHERE 
+        (job_title_short = 'Data Analyst' AND
+        job_work_from_home = TRUE AND
+        salary_year_avg IS NOT NULL)
+    ORDER BY 
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    c.name AS company_name,
+    tt.job_title AS Job_title,
+    tt.salary_year_avg AS ten_highest_paid_salary,
+    tt.job_posted_date
+FROM 
+    company_dim AS c
+INNER JOIN top_ten_highest_pay AS tt ON c.company_id = tt.company_id
+ORDER BY
+salary_year_avg DESC,
+company_name ASC;
+
+/*  - CTE are commonly used, so I tried to practicing in this case.
+    - Within the CTE Select statement, I could've omited out other columns and focus specific columns
+        to be used in final SELECT statement, but I simply would like to check if my WHERE filter clause is working properly
+        to ensure my CTE runs as I have intended.
+    - Used RIGHT JOIN initially, since I only wanted specific details only from top_ten_highest_pay table, 
+        but swapped to INNER JOIN since it disregards the unnecessary either way.
+    - In the outer query, I selected only the necessary column to display since my CTE WHERE is working as intended.
+*/
+
+
+
+--Without CTE
+SELECT
+    c.name AS company_name,
+    jp.company_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact AS jp
+INNER JOIN company_dim AS c ON jp.company_id = c.company_id
+WHERE
+    (job_title_short = 'Data Analyst' AND
+        job_work_from_home = TRUE AND
+        salary_year_avg IS NOT NULL)
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
+
+
+/* me:  Above query is a straight-forward and direct approach.
+        Useful for simple queries, assuming if not being referenced to a lot.
+*/
